c9aafc9187f2e671fc94c41aed4c039760afafce
Ray Teoh <44644189+RayTeoh@users.noreply.github.com>
2019-09-04 15:35:51 +0800

    Add files via upload

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+List<String> images = [
+  "assets/images/jogon.jpg",
+  "assets/images/rehydrated.jpg",
+  "assets/images/bookbinging.jpg",
+  "assets/images/sunnysideup.jpg",
+  "assets/images/sleepin.jpg",
+];
+
+List<String> title = [
+  "Jog On",
+  "Rehydrated",
+  "Book Binging",
+  "Sunny Side Up",
+  "Sleep In",
+];
